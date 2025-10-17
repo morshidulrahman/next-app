02c0f4fc521a5f9e4fed43b57375522cdff8e4ca
Tarek <abdullahalmomintarek@gmail.com>
2025-10-18 01:35:53 +0600

    Add download

---
+++ src/app/download/page.js
@@ -0,0 +1,12 @@
+import Download from '@/components/Download';
+import React from 'react';
+
+const DownloadPage = () => {
+    return (
+        <>
+            <Download />
+        </>
+    );
+};
+
+export default DownloadPage;
